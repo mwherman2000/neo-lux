37bb1a62d0fd1fe291143c74dc3c7cda0e938f00
phetter <fet@fet>
2018-03-06 13:52:39 -0500

    encrypted wif and private key generation example and support

--- NeoUnity/Assets/Scripts/NeoDemo.cs
+++ NeoUnity/Assets/Scripts/NeoDemo.cs
@@ -1,72 +1,126 @@
-﻿using System.Collections;
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UI;
 
 using NeoLux;
+using Neo;
 using Neo.Cryptography;
+using Neo.VM;
 
-/* about wallet.db3 
- * password is test
- * address is AH78bx9sfVKR4cPaC527ktaDRjtwKNur8B
- * private key is a9e2b5436cab6ff74be2d5c91b8a67053494ab5b454ac2851f872fb0fd30ba5e
- */
 
 public class NeoDemo : MonoBehaviour {
 
-    private enum WalletState
-    {
-        Init,
-        Sync,
-        Update,
-        Ready
-    }
+	private enum WalletState
+	{
+		Init,
+		Sync,
+		Update,
+		Ready
+	}
+	public string addressTest = "AGXUod1vZ7qugKSpZ3W1nRPRP7PwxiXYGH";
+	public string encryptedWif = "";
+	public KeyPair keys;
 
-    private KeyPair keys;
+	public Text addressLabel;
+	public Text balanceLabel;
+	public Text wifLabel;
+	public string passphrase = "test";
 
-    public Text addressLabel;
-    public Text balanceLabel;
+	public Button startBtn;
 
-    public Button startBtn;
+	private WalletState state = WalletState.Init;
+	private decimal balance;
 
-    private WalletState state = WalletState.Init;
-    private decimal balance;
+	private const string assetSymbol = "GAS";
 
-    private const string assetSymbol = "GAS";
+	void Start () {
+		byte[] privateKey = new byte[32];
 
-    void Start () {
-        this.keys = new KeyPair("a9e2b5436cab6ff74be2d5c91b8a67053494ab5b454ac2851f872fb0fd30ba5e".HexToBytes());
+		// generate a new private key
+		using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+		{
+			rng.GetBytes(privateKey);
+		}
 
-        this.addressLabel.text = keys.address;
-        this.balanceLabel.text = "Please wait, syncing balance...";        
-    }
+		// generate a key pair
+		keys = new KeyPair(privateKey);
 
-    IEnumerator SyncBalance()
-    {
-        yield return new WaitForSeconds(2);
-        var balances = NeoAPI.GetBalance(NeoAPI.Net.Test, this.keys.address);
-        this.balance = balances.ContainsKey(assetSymbol) ? balances[assetSymbol] : 0;
-        this.state = WalletState.Update;
-    }
+		// for loading specific private key strings, do it this way
+		//keys = new KeyPair("a9e2b5436cab6ff74be2d5c91b8a67053494ab5b454ac2851f872fb0fd30ba5e".HexToBytes());
 
-    void Update () {
+		addressLabel.text = keys.address;
+		balanceLabel.text = "Please WAIT, syncing balance: ...";
+		wifLabel.text =  keys.WIF; 
 
-        switch (state)
-        {
-            case WalletState.Init:
-                {
-                    state = WalletState.Sync;
-                    StartCoroutine(SyncBalance());
-                    break;
-                }
+		Debug.Log ("address: " + keys.address);
+		Debug.Log ("raw wif: " + keys.WIF);
 
-            case WalletState.Update:
-                {
-                    this.state = WalletState.Ready;
-                    this.balanceLabel.text = balance.ToString() + " "+ assetSymbol;
-                    this.startBtn.interactable = true;
-                    break;
-                }
-        }		
+		// let's encrypt the wif
+		byte[] addresshash = Encoding.ASCII.GetBytes(keys.address).Sha256().Sha256().Take(4).ToArray();
+
+		// these are the hardcoded scrypt defaults per neo-gui / neon-js: int N = 16384, int r = 8, int p = 8
+		byte[] derivedkey = SCrypt.DeriveKey(Encoding.UTF8.GetBytes(passphrase), addresshash, 16384, 8, 8, 64);
+		byte[] derivedhalf1 = derivedkey.Take(32).ToArray();
+		byte[] derivedhalf2 = derivedkey.Skip(32).ToArray();
+		byte[] encryptedkey = XOR (keys.PrivateKey, derivedhalf1).AES256Encrypt (derivedhalf2);
+		byte[] buffer = new byte[39];
+		buffer[0] = 0x01;
+		buffer[1] = 0x42;
+		buffer[2] = 0xe0;
+		Buffer.BlockCopy(addresshash, 0, buffer, 3, addresshash.Length);
+		Buffer.BlockCopy(encryptedkey, 0, buffer, 7, encryptedkey.Length);
+		encryptedWif = buffer.Base58CheckEncode();
+
+		Debug.Log ("encrypted WIF: " + encryptedWif);
+	}
+
+	IEnumerator SyncBalance()
+	{
+		yield return new WaitForSeconds(2);
+
+		Debug.Log ("getting balance for address: " + keys.address);
+
+		var balances = NeoAPI.GetBalance(NeoAPI.Net.Test, keys.address);
+		//var balances = NeoAPI.GetBalance(NeoAPI.Net.Test, addressTest);
+
+		balance = balances.ContainsKey(assetSymbol) ? balances[assetSymbol] : 0;
+		state = WalletState.Update;
+	}
+
+	void Update () {
+
+		switch (state)
+		{
+		case WalletState.Init:
+			{
+				state = WalletState.Sync;
+				StartCoroutine(SyncBalance());
+				break;
+			}
+
+		case WalletState.Update:
+			{
+				state = WalletState.Ready;
+				balanceLabel.text = balance.ToString() + " "+ assetSymbol;
+				Debug.Log ("balance: " + balanceLabel.text);
+
+				wifLabel.text = keys.WIF;
+
+				startBtn.interactable = true;
+				break;
+			}
+		}		
+	}
+
+	private static byte[] XOR(byte[] x, byte[] y)
+	{
+		if (x.Length != y.Length) throw new ArgumentException();
+		return x.Zip(y, (a, b) => (byte)(a ^ b)).ToArray();
 	}
 }
