ee246a973d1d9793824122f9d46e4bb6d38601ff
Relfos <relfos@gmail.com>
2018-03-20 09:52:06 +0000

    Clean up

--- neo-lux-demo-npc/Demo.cs
+++ neo-lux-demo-npc/Demo.cs
@@ -11,21 +11,6 @@ namespace NeoLux.Demo
             // NOTE - You can also create an API instance for a specific private net
             var api = NeoDB.ForTestNet();
 
-            // NOTE - Private keys should not be hardcoded in the code, this is just for demonstration purposes!
-            var privateKey = "a9e2b5436cab6ff74be2d5c91b8a67053494ab5b454ac2851f872fb0fd30ba5e";
-
-            Console.WriteLine("*Loading NEO address...");
-            var keys = new KeyPair(privateKey.HexToBytes());
-            Console.WriteLine("Got :"+keys.address);
-
-            // it is possible to optionally obtain also token balances with this method
-            Console.WriteLine("*Syncing balances...");
-            var balances = api.GetBalancesOf(keys.address, false);
-            foreach (var entry in balances)
-            {
-                Console.WriteLine(entry.Value + " " + entry.Key);
-            }
-
             //const string NeoDraw_ContractHash = "694ebe0840d1952b09f5435152eebbbc1f8e4b8e";
             //var response = api.TestInvokeScript(NeoDraw_ContractHash, new object[] { "getx", "user", new object[] { "100" } });
             //TEST CASE 1: object[] results = { -1 }; return results; /* WORKS */
@@ -115,47 +100,6 @@ namespace NeoLux.Demo
                                                 if (oooo != null)
                                                 {
                                                     Console.WriteLine("oooo:\t" + raIndex.ToString() + "\t" + rIndex.ToString() + "\t" + oooooIndex.ToString() + "\t" + ooooIndex.ToString() + "\t" + oooo.ToString() + "\t" + oooo.GetType().Name);
-                                                    //if (oooo.GetType().Name == "Object[]")
-                                                    //{
-                                                    //    int oooIndex = 0;
-                                                    //    foreach (object ooo in (object[])oooo)
-                                                    //    {
-                                                    //        if (ooo != null)
-                                                    //        {
-                                                    //            Console.WriteLine("ooo:\t" + raIndex.ToString() + "\t" + rIndex.ToString() + "\t" + oooooIndex.ToString() + "\t" + ooooIndex.ToString() + "\t" + oooIndex.ToString() + "\t" + ooo.ToString() + "\t" + ooo.GetType().Name);
-                                                    //            if (ooo.GetType().Name == "Object[]")
-                                                    //            {
-                                                    //                int ooIndex = 0;
-                                                    //                foreach (object oo in (object[])ooo)
-                                                    //                {
-                                                    //                    if (oo != null)
-                                                    //                    {
-                                                    //                        Console.WriteLine("oo:\t" + raIndex.ToString() + "\t" + rIndex.ToString() + "\t" + oooooIndex.ToString() + "\t" + ooooIndex.ToString() + "\t" + oooIndex.ToString() + "\t" + ooIndex.ToString() + "\t" + oo.ToString() + "\t" + oo.GetType().Name);
-                                                    //                        if (oo.GetType().Name == "Object[]" && ((object[])oo).Length > 0)
-                                                    //                        {
-                                                    //                            int oIndex = 0;
-                                                    //                            foreach (object o in (object[])oo)
-                                                    //                            {
-                                                    //                                if (o != null)
-                                                    //                                {
-                                                    //                                    Console.WriteLine("o:\t" + raIndex.ToString() + "\t" + rIndex.ToString() + "\t" + oooooIndex.ToString() + "\t" + ooooIndex.ToString() + "\t" + oooIndex.ToString() + "\t" + ooIndex.ToString() + "\t" + oIndex.ToString() + "\t" + o.ToString() + "\t" + o.GetType().Name);
-                                                    //                                    oIndex++;
-                                                    //                                }
-                                                    //                            }
-                                                    //                        }
-                                                    //                        else
-                                                    //                        {
-                                                    //                            var oo0 = (string)oo;
-                                                    //                            Console.WriteLine("oo0:\t" + raIndex.ToString() + "\t" + rIndex.ToString() + "\t" + oooooIndex.ToString() + "\t" + ooooIndex.ToString() + "\t" + oooIndex.ToString() + "\t" + ooIndex.ToString() + "\t" + oo0.ToString() + "\t" + oo0.GetType().Name);
-                                                    //                        }
-                                                    //                        ooIndex++;
-                                                    //                    }
-                                                    //                }
-                                                    //            }
-                                                    //            oooIndex++;
-                                                    //        }
-                                                    //    }
-                                                    //}
                                                     ooooIndex++;
                                                 }
                                             }
@@ -173,31 +117,6 @@ namespace NeoLux.Demo
 
             Console.WriteLine("Press Enter to Exit...");
             Console.ReadLine();
-
-            // TestInvokeScript let's us call a smart contract method and get back a result
-            // NEP5 https://github.com/neo-project/proposals/issues/3
-
-            api = NeoDB.ForMainNet();
-            var redPulse = api.GetToken("RPX");
-
-            // you could also create a NEP5 from a contract script hash
-            //var redPulse_contractHash = "ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9";
-            //var redPulse = new NEP5(api, redPulse_contractHash);
-
-            Console.WriteLine("*Querying Symbol from RedPulse contract...");
-            //var response = api.TestInvokeScript(redPulse_contractHash, "symbol", new object[] { "" });
-            //var symbol = System.Text.Encoding.ASCII.GetString((byte[])response.result);
-            var symbol = redPulse.Symbol;
-            Console.WriteLine(symbol); // should print "RPX"
-
-            // here we get the RedPulse token balance from an address
-            Console.WriteLine("*Querying BalanceOf from RedPulse contract...");
-            //var balance = api.GetTokenBalance("AVQ6jAQ3Prd32BXU5r2Vb3QL1gYzTpFhaf", "RPX");
-            var balance = redPulse.BalanceOf("AVQ6jAQ3Prd32BXU5r2Vb3QL1gYzTpFhaf");
-            Console.WriteLine(balance);
-
-            Console.WriteLine("Press any key to quit...");
-            Console.ReadKey();
         }
     }
 }
